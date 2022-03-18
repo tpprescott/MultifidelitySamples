b2aa8dad176552a07866df5f5f3976ec5f915908
Tom Prescott (Turing) <tprescott@turing.ac.uk>
2022-03-18 12:08:17 +0000

    Split information from summaries and simulation data; explicit distinction between Tractable and SimulationBased likelihoods

--- src/MultifidelitySamples.jl
+++ src/MultifidelitySamples.jl
@@ -2,11 +2,15 @@ module MultifidelitySamples
 
 using Distributions, Distributed
 
-export AbstractLikelihood, PlaceholderLikelihood
+export AbstractLikelihood, AbstractSimulationBasedLikelihood, AbstractTractableLikelihood
+export PlaceholderLikelihood
+export AbstractSummaryInformation, SummaryInformationPath
+export AbstractSimulationData, SimulationDataPath
+export NullSimulationData, BaselineSummaryInformation
 export MultifidelityPathLikelihood
 export MultifidelityPathTree, MultifidelitySample
 
-export evaluate, burden
+export evaluate, burden, ESS
 export weighted_mean
 export cost_functional
 
@@ -20,31 +24,97 @@ Optionally, they need to also implement `evaluate(::L, θ, y...)` to allow for c
 """
 abstract type AbstractLikelihood end
 
+
 """
-    evaluate(::AbstractLikelihood, θ)
+    AbstractSimulationBasedLikelihood <: AbstractLikelihood
+
+An abstract type corresponding to a monofidelity simulation-based likelihood.
+In contradistinction to `AbstractTractableLikelihood`.
+"""
+abstract type AbstractSimulationBasedLikelihood <: AbstractLikelihood end
+
+
+"""
+    AbstractTractableLikelihood <: AbstractLikelihood
+
+An abstract type corresponding to a monofidelity likelihood that is tractable without resorting to simulations.
+In contradistinction to `AbstractSimulationBasedLikelihood`.
+"""
+abstract type AbstractTractableLikelihood <: AbstractLikelihood end
+
+
+"""
+    AbstractSimulationData
+
+An abstract type corresponding to the simulation produced in evaluating any `AbstractSimulationBasedLikelihood`.
+This simulation data allows for coupling between evaluations of `AbstractSimulationBasedLikelihood`.
+"""
+abstract type AbstractSimulationData end
+struct NullSimulationData <: AbstractSimulationData end
+function Base.show(io::IO, ::NullSimulationData)
+    print(io, "∅")
+end
+
+"""
+    AbstractSummaryInformation
+
+An abstract type corresponding to summary information used to determine whether to simulate downstream
+"""
+abstract type AbstractSummaryInformation end
+struct BaselineSummaryInformation <: AbstractSummaryInformation
+    ω::Float64
+end
+
+"""
+    evaluate(::AbstractLikelihood, θ, ξ::AbstractSimulationData...)
 
 Define for concrete subtypes of `AbstractLikelihood`.
-Return `(ω::Float64, v::Vector{Float64})` where
-- `ω::Float64` represents the likelihood (or likelihood-free estimate of the likelihood)
-- `v::Vector{Float64}` can be used for coupling between likelihoods and in any mean function `μ` used to define a `MultifidelityPathLikelihood`
+Return `(ω::Real, y::Y, ξ::Ξ)` for `Y <: AbstractSummaryInformation` and `Ξ <: AbstractSimulationData`.
+Components are
+- `ω::Real` represents the evaluated likelihood, or simulation-based estimate of the likelihood.
+- `y::Y` is to be used for in any mean function `μ` used to define a `MultifidelityPathLikelihood`.
+- `ξ::Ξ` is to be used for coupling calls of `evaluate` between concrete subtypes of `AbstractSimulationBasedLikelihood`.
+
+The value relies on implementing `_evaluate(L::AbstractLikelihood, θ, ξ...)` with a default fallback of `_evaluate(L, θ)` - that is, ignoring `ξ`.
+We take each `ξ_i ∈ ξ` in turn with a default fallback of ignoring it,
+so need to implement `_evaluate(L, θ, ξ_parent::AbstractSimulationData, ξ_ancestors::AbstractSimulationData...)`
+for coupling a single `AbstractSimulationData` to the evaluation of `L`.
+
+If `L isa AbstractTractableLikelihood` then we ignore all `ξ`, and produce `BaselineSummaryInformation``and `NullSimulationData` from output.
+
+If `L isa AbstractSimulationBasedLikelihood` we require `_evaluate(L, θ, ξ...)` to output an instance of an `AbstractSummaryInformation` and `AbstractSimulationData` as well as `ω::Real`.
 """
-function evaluate(::AbstractLikelihood, θ) end
-evaluate(L::AbstractLikelihood, θ, parent, ancestors...) = evaluate(L, θ, ancestors...)
+function evaluate(L::AbstractTractableLikelihood, θ, ξ::AbstractSimulationData...)
+    ω = _evaluate(L, θ)
+    return ω, BaselineSummaryInformation(ω), NullSimulationData()
+end
+function evaluate(L::AbstractSimulationBasedLikelihood, θ, ξ::AbstractSimulationData...)
+    out = _evaluate(L, θ, ξ...)
+    if out isa Tuple{Real, AbstractSummaryInformation, AbstractSimulationData}
+        return out
+    else
+        error("Need `_evaluate(::AbstractSimulationBasedLikelihood, θ)` to output `Tuple{Real, AbstractSummaryInformation, AbstractSimulationData}`!")
+    end
+end
+
+function _evaluate(L::AbstractLikelihood, θ, ξ_parent::AbstractSimulationData, ξ_ancestors::AbstractSimulationData...)
+    return _evaluate(L, θ, ξ_ancestors...)
+end
 
 
 """
-    PlaceholderLikelihood(ω::Float64) <: AbstractLikelihood
+    PlaceholderLikelihood(ω::Float64) <: AbstractTractableLikelihood
 
 A concrete likelihood to enable quick checking that code compiles and executes.
 The likelihood is specified as `ω`.
-The 
+The parameter space is the Real line.
 """
-struct PlaceholderLikelihood <: AbstractLikelihood
+struct PlaceholderLikelihood <: AbstractTractableLikelihood
     ω::Float64
 end
-function evaluate(L::PlaceholderLikelihood, θ::Real)
+function _evaluate(L::PlaceholderLikelihood, θ::Real)
     sleep(0.0001 * θ)
-    L.ω, [L.ω]
+    L.ω
 end
 
 
@@ -55,8 +125,16 @@ Contains
 - `L_hi::HI` as high-fidelity likelihood.
 - `L_lo::LO` as a tuple (i.e. ordered list) of low-fidelity likelihoods to be sequentially evaluated as approximations of `L_hi`.
 - `μ::M` as a function determining the (Poisson) mean of evaluations of each likelihood.
-For `LVL` in `0:(N-1)` the function `μ(θ, y::Vararg{Vector{Float64}, LVL})` determines the Poisson mean of how many evaluations of `L_lo[LVL+1]` we make, conditioned on all preceding evaluations and `θ`
-At the final level, the function `μ(θ, y::Vararg{Vector{Float64}, N})` determines the Poisson mean of how many evaluations of 'L_hi' we make, conditioned on all preceding evaluations and `θ`.
+
+For `LVL` in `0:(N-1)` the function
+`μ(θ, y::Vararg{AbstractSummaryInformation, LVL})`
+determines the Poisson mean of how many evaluations of `L_lo[LVL+1]` we make,
+conditioned on `θ` and the `AbstractSummaryInformation` output from all preceding evaluations.
+
+At the final level, the function
+`μ(θ, y::Vararg{AbstractSummaryInformation, N})`
+determines the Poisson mean of how many evaluations of 'L_hi' we make, again
+conditioned on `θ` and the `AbstractSummaryInformation` output from all preceding evaluations.
 """
 struct MultifidelityPathLikelihood{N, LO<:NTuple{N, AbstractLikelihood}, HI<:AbstractLikelihood, M}
     L_hi::HI
@@ -75,39 +153,38 @@ function Base.show(io::IO, L_ρ::MultifidelityPathLikelihood{N}) where N
 end
 
 
+SummaryInformationPath{LVL} = NTuple{LVL, AbstractSummaryInformation}
+SimulationDataPath{LVL} = NTuple{LVL, AbstractSimulationData}
 """
-    MultifidelityPathTree{LVL, Θ, T<:MultifidelityPathTree{LVL+1, Θ}}
+    MultifidelityPathTree{LVL, Θ, Y <: SummaryInformationPath{LVL}, Ξ <: SimulationDataPath{LVL}, T<:MultifidelityPathTree{LVL+1, Θ}}
 
 Contains
 - `ω::Float64` as the evaluated likelihood at level `LVL`
-- `z::Tuple{Θ, Vararg{Vector{Float64}, LVL}}` as the accumulated information from this and preceding levels, and `θ`.
+- `θ::Θ` as the parameter value
+- `y::Y` as the accumulated `AbstractSummaryInformation` from this and preceding levels (most recent first).
+- `ξ::Ξ` as the accumulated `AbstractSimulationData` from this and preceding levels (most recent first).
 - `c::Float64` as the computational burden of evaluating the likelihood `ω` (i.e. the simulation burden, for likelihood-free inference).
 - `μ::Float64` as the computed mean for the Poisson number of correction trees to generate.
 - `children::T` as a vector of correction trees, rooted at the next level.
 """
-struct MultifidelityPathTree{LVL, Θ, T}
+struct MultifidelityPathTree{LVL, Θ, Y <: SummaryInformationPath{LVL}, Ξ <: SimulationDataPath{LVL}, T}
     ω::Float64
-    z::Tuple{Θ, Vararg{Vector{Float64}, LVL}}
+    θ::Θ
+    y::Y
+    ξ::Ξ
     c::Float64
     μ::Float64
     children::Vector{T}
-    function MultifidelityPathTree(ω, z::Tuple{Θ, Vararg{Vector{Float64}, LVL}}, c, μ, children::Vector{T}) where {LVL, Θ, T}
+    function MultifidelityPathTree(ω, θ::Θ, y::Y, ξ::Ξ, c, μ, children::Vector{T}) where {Θ, Y <: SummaryInformationPath{LVL}, Ξ <: SimulationDataPath{LVL}, T} where LVL
         T <: MultifidelityPathTree{LVL+1, Θ} || error("Level mismatch")
-        new{LVL, Θ, T}(ω, z, c, μ, children)
+        new{LVL, Θ, Y, Ξ, T}(ω, θ, y, ξ, c, μ, children)
     end
 end
 function Base.show(io::IO, t::MultifidelityPathTree)
     sep = ", "
-    print(io, "MultifidelityPathTree(", t.ω, sep, t.z, sep, t.c, sep, t.μ, sep, length(t.children), " children)")
+    print(io, "MultifidelityPathTree(", t.ω, sep, t.θ, sep, t.y, sep, t.ξ, sep, t.c, sep, t.μ, sep, length(t.children), " children)")
 end
 
-
-"""
-    get_θ(T::MultifidelityPathTree)
-
-Return the parameter value of the tree `T`.
-"""
-get_θ(T::MultifidelityPathTree) = T.z[1]
 
 
 """
@@ -115,7 +192,7 @@ get_θ(T::MultifidelityPathTree) = T.z[1]
 
 A vector of trees rooted at level zero: `Vector{MultifidelityPathTree{0, Θ, T}}`
 """
-MultifidelitySample{Θ, T <: MultifidelityPathTree{1, Θ}} = Vector{MultifidelityPathTree{0, Θ, T}}
+MultifidelitySample{Θ, Y, Ξ, T <: MultifidelityPathTree{1, Θ}} = Vector{MultifidelityPathTree{0, Θ, Y, Ξ, T}}
 
 
 """
@@ -141,19 +218,18 @@ We wrap `ω` and `c` and `z = (θ, y...)` in this tree.
 We use `z` as the argument to `L_ρ.μ` to calculate the mean number, `μ`, of correction trees to generate (which gets wrapped also).
 Each tree at the next level is generated in turn and stored in `children`, before the completed tree is returned.
 """
-function MultifidelityPathTree(L_ρ::MultifidelityPathLikelihood{N}, ω, c, θ::Θ, ancestors::Vararg{Vector{Float64}, LVL}) where {Θ, N, LVL}
-    z = (θ, ancestors...)
-    μ = LVL > N ? 0.0 : L_ρ.μ(z...)
-    tree = MultifidelityPathTree(ω, z, c, μ, MultifidelityPathTree{LVL+1, Θ}[])
+function MultifidelityPathTree(L_ρ::MultifidelityPathLikelihood{N}, ω, θ::Θ, c, y::Y = (), ξ::Ξ = ()) where {N, Θ, Y<:SummaryInformationPath{LVL}, Ξ<:SimulationDataPath{LVL}} where LVL
+    μ = LVL > N ? zero(Float32) : L_ρ.μ(θ, y...)
+    tree = MultifidelityPathTree(ω, θ, y, ξ, c, μ, MultifidelityPathTree{LVL+1, Θ}[])
     
     M::Integer = LVL > N ? 0 : rand(Poisson(μ))
     if M > 0
         L_next = LVL==N ? L_ρ.L_hi : L_ρ.L_lo[LVL+1]
         for _m in 1:M
-            timed_likelihood = @timed evaluate(L_next, θ, ancestors...)
-            ω_m, y_m = timed_likelihood.value
+            timed_likelihood = @timed evaluate(L_next, θ, ξ...)
+            ω_m, y_m, ξ_m = timed_likelihood.value
             c_m = timed_likelihood.time
-            push!(tree.children, MultifidelityPathTree(L_ρ, ω_m, c_m, θ, y_m, ancestors...))
+            push!(tree.children, MultifidelityPathTree(L_ρ, ω_m, θ, c_m, (y_m, y...), (ξ_m, ξ...)))
         end
     end
 
@@ -173,9 +249,9 @@ function MultifidelitySample(L_ρ::MultifidelityPathLikelihood, prior, N::Intege
     @info (isone(n) ? "Only 1 worker: look at parallelism." : "$(nworkers()) workers!")
     S = pmap(1:N) do _i
         timed_prior_sample = @timed rand(prior)
-        c_0 = timed_prior_sample.time
+        c0_i = timed_prior_sample.time
         θ_i = timed_prior_sample.value
-        T_i = MultifidelityPathTree(L_ρ, 0.0, c_0, θ_i)
+        MultifidelityPathTree(L_ρ, 0.0, θ_i, c0_i)
     end
     return S
 end
@@ -191,7 +267,7 @@ function weighted_mean(G, S::MultifidelitySample)
     den = 0.0
     for T in S
         ω = evaluate(T)
-        Gθ = G(get_θ(T))
+        Gθ = G(T.θ)
         num += ω*Gθ
         den += ω
     end
@@ -218,6 +294,18 @@ end
 Returns the total observed computational burden of the Monte Carlo sample `S` of multifidelity trees.
 """
 burden(S::MultifidelitySample) = sum(burden, S)
+
+
+"""
+    ESS(S::MultifidelitySample)
+
+Returns the effective sample size of `S`.
+"""
+function ESS(S::MultifidelitySample)
+    ω = evaluate.(S)
+    return sum(ω)^2 / sum(abs2, ω)
+end
+
 
 function _burden_functional(μ) 
     function _burden(T::MultifidelityPathTree)
